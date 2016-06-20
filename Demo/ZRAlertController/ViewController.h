bc9133feee74457670e17106e15ebdc289a12fdd
victor <victorjon@163.com>
2016-06-20 16:48:33 +0800

    Add demo for using

---
+++ Demo/ZRAlertController/ViewController.h
@@ -0,0 +1,19 @@
+//
+//  ViewController.h
+//  ZRAlertController
+//
+//  Created by Victor John on 4/28/16.
+//  Copyright © 2016 com.xiaoruigege. All rights reserved.
+//
+//  https://github.com/VictorZhang2014/AlertView-Demo-for-iOS
+//  An open source library for iOS in Objective-C that is being compatible with iOS 7.0 and later.
+//  Its main function that wrapped UIAlertView and UIAlertController that are easier to call.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Demo/ZRAlertController/ViewController.m
@@ -0,0 +1,61 @@
+//
+//  ViewController.m
+//  ZRAlertController
+//
+//  Created by Victor John on 4/28/16.
+//  Copyright © 2016 com.xiaoruigege. All rights reserved.
+//
+//  https://github.com/VictorZhang2014/AlertView-Demo-for-iOS
+//  An open source library for iOS in Objective-C that is being compatible with iOS 7.0 and later.
+//  Its main function that wrapped UIAlertView and UIAlertController that are easier to call.
+//
+
+#import "ViewController.h"
+#import "ZRAlertController.h"
+
+@interface ViewController ()
+- (IBAction)firstButton:(id)sender;
+- (IBAction)SecondButton:(id)sender;
+- (IBAction)ThirdButton:(id)sender;
+- (IBAction)fourthButton:(id)sender;
+ 
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+- (IBAction)firstButton:(id)sender
+{
+    [[ZRAlertController defaultAlert] alertShow:self title:@"Note" message:@"This is an empty message!" okayButton:@"Ok"];
+} 
+
+- (IBAction)SecondButton:(id)sender
+{
+    [[ZRAlertController defaultAlert] alertShow:self title:@"Note" message:@"This is an empty message!" cancelButton:@"Cancel" okayButton:@"Ok" okayHandler:^{
+        NSLog(@"You clicked the okay button.");
+    } cancelHandler:^{
+        NSLog(@"You clicked the cancel button.");
+    }];
+}
+
+- (IBAction)ThirdButton:(id)sender
+{
+    [[ZRAlertController defaultAlert] alertShow:self title:@"Note" message:@"This is an explanation message!" cancelButton:@"Cancel" okayButton:@"Okay" alertStyle:ZRAlertStyleSecureTextInput placeHolder:@"Type any character" okayHandler:^(UITextField *textFiled) {
+        NSLog(@"Your input of textFiled is %@.", textFiled.text);
+    } cancelHandler:^(UITextField *textFiled) {
+        NSLog(@"Your input of textFiled is %@.", textFiled.text);
+    }];
+}
+
+- (IBAction)fourthButton:(id)sender {
+    [[ZRAlertController defaultAlert] alertShow:self title:@"Note" message:@"This is an explanation message!" cancelButton:@"Cancel" okayButton:@"Ok" alertStyle:ZRAlertStyleLoginAndPasswordInput placeHolder1:@"Type an account" placeHolder2:@"Type a passcode" sureHandler:^(UITextField *textFiled1, UITextField *textFiled2) {
+        NSLog(@"Your input of Sure textFiled1.text = %@, textFiled2.text = %@. ", textFiled1.text, textFiled2.text);
+    } abolishHandler:^(UITextField *textFiled1, UITextField *textFiled2) {
+         NSLog(@"Your input of Abolish textFiled1.text = %@, textFiled2.text = %@. ", textFiled1.text, textFiled2.text);
+    }];
+}
+@end
